6f8de0e5cd9954ec2e7a7c74f1d8400b524f7cfc
Berkay YİĞİT <berkayyigit@anadolu.edu.tr>
2022-04-16 20:15:08 +0300

    [MW05] Improve `Truck Rain`

--- src/Extensions/Game/MW05/Effects/TruckRain.hpp
+++ src/Extensions/Game/MW05/Effects/TruckRain.hpp
@@ -67,7 +67,7 @@ namespace Extensions::Game::MW05::Effects {
         position.z += 10.0f;
         direction.x *= -1.0f;
 
-        auto* new_pvehicle = OpenMW::PVehicle::Construct(OpenMW::VehicleParams(OpenMW::DriverClass::None, OpenMW::Attrib::StringToKey("cs_semi"), direction,
+        auto* new_pvehicle = OpenMW::PVehicle::Construct(OpenMW::VehicleParams(OpenMW::DriverClass::None, OpenMW::Attrib::StringToKey("semi_con"), direction,
                                                                                position, nullptr, OpenMW::eVehicleParamFlags::None)) |
                              OpenMW::PVehicleEx::ValidatePVehicle;
         if (!new_pvehicle) return;
